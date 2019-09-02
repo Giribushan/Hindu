e2a91a8241f20176de82ec4d6617247e32d775b4
gcheru200 <Giri_Cheruvu@comcast.com>
2019-09-02 16:04:46 +0530

    Updates

---
+++ precache-manifest.14df4d540d15948f5765b1211ea97223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8b663af8a2e93f3b4cd",
+    "url": "/Hindu/static/css/main.44fc64fa.chunk.css"
+  },
+  {
+    "revision": "c8b663af8a2e93f3b4cd",
+    "url": "/Hindu/static/js/main.c8b663af.chunk.js"
+  },
+  {
+    "revision": "a073a30a44f49cd573e9",
+    "url": "/Hindu/static/js/runtime~main.a073a30a.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/Hindu/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "bb7ad34fe975f222806e07f9ce0d4f49",
+    "url": "/Hindu/index.html"
+  }
+];
